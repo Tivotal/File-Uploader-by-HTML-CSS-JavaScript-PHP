daabb0ea4742f53fc482b35c60670c616079fc39
Tivotal <121335284+Tivotal@users.noreply.github.com>
2023-04-21 15:37:48 +0530

    Update upload.php

--- php/upload.php
+++ php/upload.php
@@ -1,1 +1,13 @@
+<?php 
+//getting file name
+$file_name = $_FILES['file']['name'];
 
+//getting temp name of the file
+$tmp_name = $_FILES['file']['tmp_name'];
+
+//making file name dynamic by adding current time stamp to file name
+$name_updated = time().$file_name;
+
+//moving file to specified folder
+move_uploaded_file($tmp_name, "files/".$name_updated);
+?>
